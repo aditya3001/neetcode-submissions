e30b2fda4f1675e8d65610601c4a12505431c2aa
Aditya Jain <68020519+aditya3001@users.noreply.github.com>
2026-08-25 21:24:06 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int start = 0;
+        int end = 0;
+        int profit = 0;
+
+        while (end < prices.length) {
+            if (prices[end] > prices[start]) {
+                profit = Math.max(prices[end] - prices[start], profit);
+            } else {
+                start = end;
+            }
+            end++;
+
+        }
+        return profit;
+
+    }
+}

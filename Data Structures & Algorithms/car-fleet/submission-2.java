8c2817209eb4b4e2ba1f423594b695fe37b1b696
Aditya Jain <68020519+aditya3001@users.noreply.github.com>
2026-09-04 17:21:26 +0530

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        
+        int[][] combArray = new int[position.length][3];
+        for (int i = 0 ; i < position.length ; i++) {
+            combArray[i][0] = position[i];
+            combArray[i][1] = speed[i];
+        }
+        Arrays.sort(combArray, (k1, k2) -> {
+            return Integer.compare(k2[0], k1[0]);
+        });
+        Stack<Double> intStack = new Stack<>();
+        for(int j = 0 ; j < position.length; j++) {
+            double val = ((double) target - (double) combArray[j][0] )/(double) combArray[j][1];
+            if (intStack.isEmpty()) {
+                intStack.push(val);
+                continue;
+            }
+            if (val > intStack.peek()) {
+                intStack.push(val);
+            }
+        }
+        System.out.println(intStack);
+
+        return intStack.size();
+        
+    }
+}
